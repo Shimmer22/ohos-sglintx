7a2e1198d1d8789c5c768c42b6b4dd8c93b6dcf7
Shimmer22 <1914689536@qq.com>
2026-01-29 12:26:01 +0800

    Fix: Enable experimental extensions for AFLAGS

--- device/board/Humpback/SGLinTx/kernel/build_kernel.sh
+++ device/board/Humpback/SGLinTx/kernel/build_kernel.sh
@@ -91,6 +91,7 @@ sed -i 's/KBUILD_AFLAGS += -mabi=lp64/KBUILD_AFLAGS += --target=riscv64-linux-oh
 
 # 7. Enable experimental extensions for Vector 0.7 (v0p7 requires check disabled)
 sed -i 's/KBUILD_CFLAGS += -march=/KBUILD_CFLAGS += -menable-experimental-extensions -march=/g' arch/riscv/Makefile
+sed -i 's/KBUILD_AFLAGS += -march=/KBUILD_AFLAGS += -menable-experimental-extensions -march=/g' arch/riscv/Makefile
 # -------------------------------------------------------------------
 
 # 2. HDF Patch
